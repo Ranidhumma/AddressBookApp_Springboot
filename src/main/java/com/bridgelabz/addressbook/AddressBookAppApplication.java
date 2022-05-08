d5e31ccd7aa3f621a296d283575f35b659a1f539
Ranidhumma <ranidhumma8@gmail.com>
2022-05-08 20:23:40 +0530

    Created AddressBook SpringProject

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook Application");
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
