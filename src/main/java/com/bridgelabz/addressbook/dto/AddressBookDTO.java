71ba0e7beae4285004c5a0c8c35918c568db7f27
Ranidhumma <ranidhumma8@gmail.com>
2022-05-11 15:08:54 +0530

    Added Regex pattern for Name

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.addressbook.dto;
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 public @Data class AddressBookDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "name is Invalid")
     public String name;
     public String phNumber;
 
